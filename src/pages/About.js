02f6d3e44309cb194ec930c160207c8338bd3a99
Mati Joseph <crucialniccur@gmail.com>
2025-04-23 05:36:10 +0300

    create About and Login pages and add boilerplate code

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <header>{/* space for navbar */}</header>
+      <main>
+        <h1>This is my about component!</h1>
+      </main>
+    </>
+  );
+};
+
+export default About;
