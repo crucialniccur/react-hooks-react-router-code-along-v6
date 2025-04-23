987d79c5602b3658ea5c0d5ec2159e0313b67fa8
Mati Joseph <crucialniccur@gmail.com>
2025-04-23 06:05:15 +0300

    create an error page and add it to the routes in index.js

--- src/index.js
+++ src/index.js
@@ -5,23 +5,28 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/about",
     element: <About />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
     element: <Login />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/profile/:id",
     element: <UserProfile />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
