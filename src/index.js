e8813d838e0fc12cf078d1b60ece5d08b6da2410
Mati Joseph <crucialniccur@gmail.com>
2025-04-23 05:30:38 +0300

    create a router constant and pass in an array of objects with paths

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
